eb58a80a21880f3b30600e86905b0a29e6d39468
olegpixel <oleg.yevik@gmail.com>
2018-11-13 17:54:52 +1300

    Url hash generation and save

---
+++ models/link.js
@@ -0,0 +1,43 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const Link = mongoose.model('Link', new mongoose.Schema({
+  URL: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 2048
+  },
+  shortHash: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 6
+  },
+  clickCounter: {
+    type: Number
+  }
+}))
+
+function generateHash() {
+    const minInt = 14776336
+    const maxInt = 525649985
+    const randomInt = Math.floor(Math.random() * (maxInt - minInt)) + minInt
+    return randomInt.toString(36)
+}
+
+// function validateCustomer(customer) {
+//   const schema = {
+//     name: Joi.string().min(5).max(50).required(),
+//     phone: Joi.string().min(5).max(50).required(),
+//     isGold: Joi.boolean()
+//   };
+
+//   return Joi.validate(customer, schema);
+// }
+
+exports.Link = Link
+exports.generateHash = generateHash
+
+
+// exports.validate = validateCustomer;
